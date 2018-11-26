410514e5c82f346b7f70957ee074ae2d2390f893
Hugo Martinez <hugo@yeezers.com>
2018-11-26 13:24:17 +0100

    Add new messga and create message events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User connected');
 
+    // socket.emit('newEmail', {
+    //     from: 'mike@pepe.com',
+    //     text: 'Hey, whats on?'
+    // });
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
+
+    socket.emit('newMessage', {
+        from: 'Hugo',
+        text: 'Hey, can yu meet up at 6pm?',
+        createdAt: 123123
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
