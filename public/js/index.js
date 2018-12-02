30a9378d5b67556a2e5c555614d4374d66656726
Hugo Martinez <hugo@yeezers.com>
2018-12-02 09:21:32 +0100

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -27,22 +27,16 @@ socket.on('newLocationMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
-// socket.emit('createMessage', {
-//     from: 'Rebeca',
-//     text: 'Hi over there!'
-// //Adding acknowledgement to the Client    
-// }, function (data) {
-//     console.log('Got it!', data);
-// });
-
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
    socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
    }, function () {
-
+    messageTextbox.val('');
    });
 });
 
@@ -52,14 +46,17 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser');        
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position);
+        locationButton.removeAttr('disabled').text('Send location');
 
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
